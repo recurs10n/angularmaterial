da87b4b071aef2029778ed2535b4e1f9cfbd9928
Daniel Brown <dsjbrown@gmail.com>
2018-04-17 22:57:12 -0400

    Post Displaying Real Data

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material';
 
 const SMALL_WIDTH_BREAKPOINT = 720;
 
@@ -11,12 +16,28 @@ export class SidenavComponent implements OnInit {
 
   private mediaMatcher: MediaQueryList = matchMedia( `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)` );
 
-  constructor(zone: NgZone ) {
+  users: Observable<User[]>;
+
+  constructor(
+    zone: NgZone, 
+    private userService: UserService,
+    private router: Router ) 
+  {
     this.mediaMatcher.addListener( mql => 
       zone.run( () => this.mediaMatcher = mql ));
   }
 
+  @ViewChild(MatSidenav) sidenav: MatSidenav;
   ngOnInit() {
+    this.users = this.userService.users;
+    this.userService.loadAll();
+
+    this.router.events.subscribe( () => {
+      if( this.isScreenSmall() )
+      {
+        this.sidenav.close();
+      }
+    })
   }
 
   isScreenSmall(): boolean {
